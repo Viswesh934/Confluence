32eb33e99f7fcfe08b5c1383a26ac1e922cb7cf6
sigireddy viswesh <98519767+Viswesh934@users.noreply.github.com>
2025-05-27 12:56:41 +0000

    Forge quest  novice completed with new task

--- Hello-world/src/frontend/index.jsx
+++ Hello-world/src/frontend/index.jsx
@@ -1,9 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import ForgeReconciler, { Text, Heading, Lozenge } from '@forge/react';
+import { invoke, view, requestConfluence } from '@forge/bridge';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [theme, setTheme] = useState(null);
+  const [accountID, setAccountID] = useState(null);
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    const getUserInfo = async() => {
+      if(accountID) {
+        let bodyData = `{
+          "accountIds": [
+            "${accountID}"
+          ]
+        }`;
+
+        const response = await requestConfluence(`/wiki/api/v2/users-bulk`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: bodyData
+        });
+        if(response.status === 200) {
+          let data = await response.json();
+          console.log(data);
+
+          setName(data.results[0].publicName);
+        } else 
+        { 
+          console.log(response)
+        }
+      } else {
+        console.log("accountID is null")
+      }
+  }
+
+  getUserInfo();
+
+  }, [accountID]);
+
+  useEffect(() => {
+    const getTheme = async() => {
+      const context = await view.getContext();
+      console.log(context);
+      setTheme(context.theme.colorMode);
+      setAccountID(context.accountId);
+    }
+
+  getTheme();
+  }, []);
 
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
@@ -11,8 +60,9 @@ const App = () => {
 
   return (
     <>
-      <Text>Hello world!</Text>
+      <Heading size="xlarge">Hello {name ? name: 'World'}!</Heading>
       <Text>{data ? data : 'Loading...'}</Text>
+      <Text>Current theme: <Lozenge>{theme ? theme : 'Loading...'}</Lozenge></Text>
     </>
   );
 };
